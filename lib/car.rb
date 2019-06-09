ba564a91b998e479e9c66135f98cf2c57ab107e3
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-09 15:40:53 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+#need access to the Vehicle class
+
+class Car < Vehicle
+  #< to inherit Car class from Vehicle
+  def go
+    #can overwrite the #go method from Vehicle class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
